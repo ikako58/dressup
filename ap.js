a67e555ae5001b13504f12c3f63ab0090b5a7694
ikako58 <ikakodzocenidze3@gmail.com>
2023-07-03 19:37:43 +0400

    dressup 2 update

--- ap.js
+++ ap.js
@@ -14,3 +14,10 @@ function myFunction() {
       }
     }
   }
+  var modal = document.getElementById('id01');
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
